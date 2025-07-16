b87c307a847a2a97d0c7229988acf1a21e611a42
muneemgithub <muneemahmed456@gmail.com>
2025-07-16 17:36:27 +0500

    Create About and Projects Pages and Fully functional and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Components/Header'
+import About from './Components/About'
+import Projects from './Components/Projects'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
      
      <Header/>
+     <About/>
+     <Projects/>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,10 +19,10 @@ const Navbar = () => {
        <div  className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
         <img src={assets.logo} alt="" />
         <ul className='hidden md:flex gap-7 text-white '>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
-            <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+            <a href="#Home" className='cursor-pointer hover:text-gray-400'>Home</a>
+            <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
+            <a href="#Projects" className='cursor-pointer hover:text-gray-400'>Projects</a>
+            <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full cursor-pointer'>Sign Up</button>
         <img onClick={() => setShowMobileMenu(true)} src={assets.menu_icon} alt="" className='md:hidden w-7 cursor-pointer' />
