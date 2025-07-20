b89c9c064f0af677670bf632765b2924412959cf
muneemgithub <muneemahmed456@gmail.com>
2025-07-20 15:20:32 +0500

    testimonails card's setting

--- src/Components/Testimonails.jsx
+++ src/Components/Testimonails.jsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 const Testimonails = () => {
   return (
     <motion.div
-    initial={{opacity: 0, x:200}}
-      transition={{duration: 1}}
-      whileInView={{opacity: 1, x:0}}
-      viewport={{once:true}}
+      initial={{ opacity: 0, x: 200 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
       className="container mx-auto py-10 lg:px-32 w-full overflow-hidden"
       id="Testimonials"
     >
@@ -21,7 +21,7 @@ const Testimonails = () => {
       <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
         Real Stories from Thoes Who Found Home with Us
       </p>
-      <div className="flex flex-wrap justify-center gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
         {testimonialsData.map((testimonial, index) => (
           <div
             key={index}
@@ -32,12 +32,14 @@ const Testimonails = () => {
               src={testimonial.image}
               alt={testimonial.alt}
             />
-            <h2 className="text-xl text-gray-700 font-medium">{testimonial.name}</h2>
+            <h2 className="text-xl text-gray-700 font-medium">
+              {testimonial.name}
+            </h2>
             <p className="text-gray-500 mb-4 text-sm">{testimonial.title}</p>
             <div className="flex justify-center gap-1 text-red-500 mb-4">
-                {Array.from({length: testimonial.rating}, (item, index)=>(
-                    <img key={index} src={assets.star_icon} alt="" />
-                ))}
+              {Array.from({ length: testimonial.rating }, (item, index) => (
+                <img key={index} src={assets.star_icon} alt="" />
+              ))}
             </div>
             <p className="text-gray-600">{testimonial.text}</p>
           </div>
